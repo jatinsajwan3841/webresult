bc0caa122fe539ae7338a965502a4e45b9331455
jatinsajwan3841 <58560850+jatinsajwan3841@users.noreply.github.com>
2021-01-20 19:24:23 +0530

    Add files via upload

---
+++ webresult/views.py
@@ -0,0 +1,21 @@
+# views file, let's see
+
+from django.shortcuts import render 
+
+
+def index(request):
+    params = {'name': 'first django proj'}
+    return render(request, 'index.html', params)
+
+def about(request):
+    from sup import result
+    name=request.GET.get('name')
+    clgid=request.GET.get('clgid')
+    branch=request.GET.get('branch')
+    s = result(name,clgid,branch)
+    s.select(1)
+    s.select(2)
+    s.select(3)
+    
+    args = {'table' : s.display('t'), 'name' : name, 'sems' : s.display('x'), 'perc' : s.display('y')}
+    return render(request, 'output.html',args)
