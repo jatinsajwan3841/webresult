721045fd660fafb16463509b9526cfacbcac052e
jatinsajwan3841 <58560850+jatinsajwan3841@users.noreply.github.com>
2021-02-05 17:42:33 +0530

    Update views.py

--- webresult/views.py
+++ webresult/views.py
@@ -1,27 +1,34 @@
 # views file, let's see
-
 from django.shortcuts import render
+from sup import result
+from datetime import datetime
+
 
 def index(request):
     return render(request, 'index.html')
 
+
 def about(request):
-    from sup import result
-    name=request.GET.get('name')
-    branch=request.GET.get('branch')
+
+    start = datetime.now()
+
+    name = request.GET.get('name')
+    branch = request.GET.get('branch')
     try:
         name = int(name)
-    except :
+    except:
         try:
             name = name.lower()
         except:
             return render(request, 'index.html')
-    s = result(name,branch)
+    s = result(name, branch)
     if s.display('check') == 'a':
-        params = {'f' : '''<div class="alert alert-danger" role="alert">The entered data didn't matched, please try again</div>'''}
+        params = {
+            'f': '''<div class="alert alert-danger" role="alert">The entered data didn't matched, please try again</div>'''}
         return render(request, 'index.html', params)
     else:
-        args = {'table' : s.display('t'), 'name' : name, 'sems' : s.display('x'), 'perc' : s.display('y')}
+        args = {'table': s.display('t'), 'name': name, 'sems': s.display(
+            'x'), 'perc': s.display('y')}
         s.clear()
-        return render(request, 'output.html',args)
-    
+        print(datetime.now() - start)
+        return render(request, 'output.html', args)
