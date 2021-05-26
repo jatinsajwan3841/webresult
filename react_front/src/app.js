37aa11f66bddaf6edb2866220711a32fd6117d4a
jatinsajwan3841 <58560850+jatinsajwan3841@users.noreply.github.com>
2021-05-26 16:51:22 +0530

    Update app.js

--- react_front/src/app.js
+++ react_front/src/app.js
@@ -3,18 +3,20 @@ import Container from "@material-ui/core/Container";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
 import MenuItem from "@material-ui/core/MenuItem"
-import { render } from "react-dom";
 import Load from "./Load";
 import { makeStyles } from '@material-ui/core/styles';
-import { useState, useEffect } from 'react';
+import { useEffect } from 'react';
 import About from "./about";
+import Snackbar from '@material-ui/core/Snackbar';
+import SnackbarContent from '@material-ui/core/SnackbarContent';
+import DoubleArrowIcon from '@material-ui/icons/DoubleArrow';
 
 const useStyles = makeStyles((theme) => ({
     beg:{
       marginTop: theme.spacing(10),
     },
     form: {
-      width: '100%', // Fix IE 11 issue.
+      width: '100%', 
       marginTop: theme.spacing(1),
     },
     submit: {
@@ -60,44 +62,21 @@ export default function App() {
     const [branch, setbranch] = React.useState('Choose');
     const [name, setname] = React.useState('')
     const [brer, setbrer] = React.useState(false)
-    const [brhelp, setbrhelp] = React.useState('please choose your branch')
-    const [response, setresponse] = useState(0);
+    const [response, setresponse] = React.useState(0);
+    const [load, setload] = React.useState(false);
 
     
     const handleBranch = (event) => {
         setbranch(event.target.value);
-        if (event.target.value == 'CE'){
-            render(
-                <TextField type="number"
-                 required 
-                 fullWidth 
-                 id="name" 
-                 name="name"
-                 label="College-ID" 
-                 helperText="Please enter your College-ID" 
-                 onInput={handleNaam} />,
-                 document.getElementById("next")
-            )
-        }else{
-            render(
-                <TextField
-                 required 
-                 fullWidth 
-                 id="name" 
-                 name="name"
-                 label="Name" 
-                 helperText="Please enter your full name" 
-                 onInput={handleNaam} />,
-                 document.getElementById("next")
-            )
-        }
+        if (event.target.value === 'Choose') setbrer(true); else setbrer(false);
     };
+
     const handleNaam = (event) => {
         setname(event.target.value)
     };
 
-    const Response = (name, branch) =>{
-      let url = 'https://result1822.herokuapp.com/about?name=' + name + '&branch=' + branch;
+    const Response = () =>{
+      let url = 'http://127.0.0.1:8000/about?name=' + name + '&branch=' + branch;
             fetch(url)
             .then(response => response.json())
             .then(data => setresponse(data));
@@ -106,68 +85,75 @@ export default function App() {
 
     useEffect(() => {
       if(response == 0){}
-      else if(response == "nf"){
-          document.getElementById('smb').innerText = 'Submit';  
-          return(
-          render(
-          "The data didn't matched, please try again",
-          document.getElementById("error")
-          ))
-      }
-      else{
-        render(
-          <About data={response} name={name}/>,
-          document.getElementById("new")
-        )
+      else if(response === "nf") {
+        setload(false);
+        setTimeout(() => setresponse(0), 4000);
       }
   },[response]
   );
 
   const fvalidate = (event) => {
-        if (branch == 'Choose'){
-            event.preventDefault();
-            setbrhelp ('please choose correct branch');
+        event.preventDefault();
+        if (branch === 'Choose'){
             setbrer(true)
         }
         else{
-          event.preventDefault();
-          render(<Load/>,
-            document.getElementById('load')
-            )
-          document.getElementById('smb').innerText = 'loading...';
-          Response(name, branch);
+          setload(true)
+          Response();
         }
     }
 
-    return (
-      <div id="new">
-        <div id="load" />
+    return (<>
+      {response === 0 || response === "nf" ? 
+        <>
+        {load ? <Load/> : null}
         <Container maxWidth="sm" >
-            <b id="error"></b>
+              <Snackbar
+                  anchorOrigin = {{vertical : 'top', horizontal: 'center' }}
+                  open={response === "nf" ? true : false}>
+                    <SnackbarContent style={{ backgroundColor: 'rgba(255,0,0,0.7)' }} 
+                        message="The entered data didn't matched, please try again !" />
+              </Snackbar>
             <h2 className={classes.beg}> Hello {name}</h2>
             <p>This is a Django - Reactjs based project where anyone from the batch 2018-22 can have a quick reference of their performance
                  in different semesters by a table indicating minimal necessary things and a graph.</p>
-            <form className={classes.form} onSubmit={fvalidate} method="GET">
+            <form className={classes.form} onSubmit={fvalidate}>
                 <TextField name="branch"
-                id="branch" 
-                error={brer}
-                select
-                label="branch"
-                fullWidth
-                helperText= {brhelp}
-                value = {branch}
-                onChange={handleBranch} >
-                {branches.map ((option) =>(
-                    <MenuItem key={option.value} value={option.value}>
-                    {option.label}
-                    </MenuItem>
-                ))}
+                          id="branch" 
+                          error={brer}
+                          select
+                          label="branch"
+                          fullWidth
+                          helperText= {brer ? 'please choose correct branch' : 'please choose your branch' }
+                          value = {branch}
+                          onChange={handleBranch} >
+                          {branches.map ((option) =>(
+                              <MenuItem key={option.value} value={option.value}>
+                              {option.label}
+                              </MenuItem>
+                          ))}
                 </TextField>
-                <div id = "next" style={{marginTop: "10px"}}></div>
+
+                {branch !== 'Choose' ? <TextField style={{marginTop: "10px"}} type={branch === 'CE' ? "number" : "text"}
+                          required 
+                          fullWidth 
+                          id="name" 
+                          name="name"
+                          label={branch === 'CE' ? "College-ID" :  "Name"}
+                          helperText={branch === 'CE' ? "Please enter your College-ID" :  "Please enter your full name" }
+                          onInput={handleNaam} />
+                        :
+                        null}
+
                 <center>
-                <Button className={classes.submit} type="submit" variant="contained" color="primary" id="smb" >Submit</Button></center>
+                  <Button className={classes.submit} type="submit" variant="contained" color="primary" id="smb" startIcon={<DoubleArrowIcon/>} >
+                    {load ? "loading..." : "Submit" }
+                  </Button></center>                  
             </form>
-        </Container>
-      </div>
+        </Container></>
+        :
+        <About data={response} name={name}/>
+      }
+      </>
     )
 }
